ef76645e09dea3ee54595d54f582a056843532e7
Leesel Fraser <leeselfraser08@gmail.com>
2020-02-20 20:37:01 -0500

    Initial Commit

---
+++ Leesel_Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Leesel_Flashcards
+//
+//  Created by Leesel Fraser on 2/20/20.
+//  Copyright © 2020 Leesel Fraser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Leesel_FlashcardsTests/Leesel_FlashcardsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Leesel_FlashcardsTests.swift
+//  Leesel_FlashcardsTests
+//
+//  Created by Leesel Fraser on 2/20/20.
+//  Copyright © 2020 Leesel Fraser. All rights reserved.
+//
+
+import XCTest
+@testable import Leesel_Flashcards
+
+class Leesel_FlashcardsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
